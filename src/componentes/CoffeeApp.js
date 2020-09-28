0535500f2f1591cb2ab2ba3ff4c62a159f09badd
Nahuel <vnahuelnicolas@gmail.com>
2020-09-27 21:45:38 -0300

    first commit

---
+++ src/componentes/CoffeeApp.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+
+const CoffeApp = () => {
+    return ( 
+        <>
+            <h1>Coffee App</h1>
+
+            <div className="content">
+                <form id="add-cafe-form">
+
+                </form>
+                <ul id="cafe-list">
+
+                </ul>
+            </div>
+        </>      
+     );
+}
+ 
+export default CoffeApp;
